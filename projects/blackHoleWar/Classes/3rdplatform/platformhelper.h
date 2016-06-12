c6be33c9bcf7914a3816eff1b2265ba39efe4960
ximeng <mxi@bainainfo.com>
2016-06-12 15:38:04 +0800

    修复部分编译问题

--- projects/blackHoleWar/Classes/3rdplatform/platformhelper.h
+++ projects/blackHoleWar/Classes/3rdplatform/platformhelper.h
@@ -1,10 +1,12 @@
 #pragma once
+#include "../../../../cocos2dx/platform/CCPlatformConfig.h"
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
 #include <jni.h>
 #include "platform/android/jni/JniHelper.h"
 #include <cocos2d.h>
-#endif
 USING_NS_CC;
+#endif
+
 class PlatFormHelper
 {
 public:
